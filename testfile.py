02ed5c5338512d4655faf65ec2b0a252936bbac4
Henfau <henrik.fauskanger@gmail.com>
2017-11-07 11:14:04 +0100

    MergeConflictGenerator.git

--- testfile.py
+++ testfile.py
@@ -2,3 +2,13 @@
 #this is a test file, bitches
 
 print("Incomprehensible, bitches!")
+
+class Clausinator():
+
+    def __init_(self,height, weight, awesomeness):
+        self.height = height
+        self.weight = weight
+        self.awesomeness = awesomeness
+
+    def exude(self):
+        print("wololololololo")
