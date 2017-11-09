ca1985378cf6f88c9cb8a06f16d5300c9d360a64
Henfau <henrik.fauskanger@gmail.com>
2017-11-09 19:30:48 +0100

    ultrasound test 1.1

--- basic_robot/Main.py
+++ basic_robot/Main.py
@@ -1,12 +1,15 @@
 from camera import Camera
 from ultrasonic import Ultrasonic
+from motors import Motors
 from PIL import Image
 from imager2 import Imager
 import PIL
 from time import sleep
 cam = Camera()
 ultra = Ultrasonic()
+m = Motors()
+m.stop()
 while True:
-
+    ultra.reset()
     print(ultra.sensor_get_value())
     sleep(1)
