13c9f391ce12d9523ae3c10bedbdcc55928c497f
Henfau <henrik.fauskanger@gmail.com>
2017-11-09 21:39:42 +0100

    -||- motor speed to 1

--- basic_robot/Motob.py
+++ basic_robot/Motob.py
@@ -5,16 +5,17 @@ from zumo_button import ZumoButton
 class Motob:
     def __init__(self):
         self.m = Motors()
+        self.speed = 1
 
 
     def turn_right(self,amount):
         #TODO: regne på forhold mellom speed og dur
         #TODO: finne ut hvilken fart som gir mest presis styring
         #NOTE: Operasjonen varer i "amount" sekunder.
-        self.m.right(dur=amount)
+        self.m.right(speed = self.speed,dur=amount)
 
     def turn_left(self,amount):
-        self.m.left(dur = amount)
+        self.m.left(speed = self.speed,dur = amount)
 
 m = Motob()
 while True:
