4bddacfd0491714cde53f6fc7bb806db3988f68e
Claus Martinsen <clauswrm@gmail.com>
2017-11-09 23:55:53 +0100

    Testing line-following capabillities

---
+++ basic_robot/lineFollowerTest.py
@@ -0,0 +1,36 @@
+from Motob import Motob
+import reflectance_sensors
+from zumo_button import ZumoButton
+
+d = 20
+m = Motob()
+
+def calculate_error(sens_values):
+    outValue = 0
+    outValue += sens_values[0]
+    outValue += 0.6 * sens_values[1]
+    outValue -= 0.6 * sens_values[3]
+    outValue -= sens_values[4]
+    return outValue
+
+
+sens = reflectance_sensors.ReflectanceSensors()
+value = []
+
+while True:
+    ZumoButton().wait_for_press()
+
+    m.forward()
+
+    running = True
+    while running:
+        value = sens.update()
+        error = calculate_error(value)
+        if error > 0:
+            m.turn_left(error * 20)
+        else:
+            m.turn_right(error * 20)
+
+    m.stop()
+
+
