f2d05e7791504713062ee4c057f61572759a0f7c
Henfau <henrik.fauskanger@gmail.com>
2017-11-09 21:30:40 +0100

    Motob test

--- basic_robot/Motob.py
+++ basic_robot/Motob.py
@@ -1,3 +1,23 @@
+from motors import Motors
+from time import sleep
 class Motob:
     def __init__(self):
-        pass
+        self.m = Motors()
+
+
+    def turn_right(self,amount):
+        #TODO: regne på forhold mellom speed og dur
+        #TODO: finne ut hvilken fart som gir mest presis styring
+        #NOTE: Operasjonen varer i "amount" sekunder.
+        self.m.right(dur=amount)
+
+    def turn_left(self,amount):
+        self.m.left(dur = amount)
+
+m = Motob()
+
+for i in range(0,5):
+    m.turn_right(1)
+    sleep(1)
+    m.turn_left(1)
+    sleep(1)
