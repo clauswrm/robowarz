e500e40db90f944ae3cd7f66d6f895cad524bb10
Ask Somervoll <asksom@gmail.com>
2017-11-14 10:32:56 +0100

    color avg now works, uploading the fixed version

--- basic_robot/sensobs/camobs.py
+++ basic_robot/sensobs/camobs.py
@@ -1,9 +1,10 @@
 from camera import Camera
-from imager2 import getpixel
+# from imager2 import getpixel
 from time import sleep
+from PIL import Image
 
 
-class camob:
+class Camob:
     def __init__(self):
         self.c = Camera()
 
@@ -12,6 +13,7 @@ class camob:
 
     '''iterer over alle pixlene i listen og legger sammen verdiene, deler deretter på antall pixler'''
     ''' returnerer deretter gjenomsnittet av alle verdiene'''
+
     def get_avg_col(self):
         im = self.c.sensor_get_value
 
@@ -19,7 +21,7 @@ class camob:
         for i in range(self.c.img_height):
             for j in range(self.c.img_width):
                 for k in range(0, 3):
-                    return_list[k] += im.getpixel(j, i)[k]
+                    return_list[k] += im.getpixel((j, i))[k]
         for l in range(0, 3):
             return_list[l] = return_list[l] // (self.c.img_height * self.c.img_width)
         return return_list
@@ -31,9 +33,25 @@ class camob:
             self.c.update
             sleep(1)
 
-
-
     def find_red_circle(self):
         '''Skal finne den røde sirkelen, skriver resten av koden etter jeg har testet noen numeriske verdier'''
         if True:
             pass
+
+'''
+im = Image.open('image.jpg')
+print(im.getpixel((1, 1)))
+
+return_list = [0, 0, 0]
+for i in range(0, 96):
+    for j in range(0, 128):
+        for k in range(0, 3):
+            return_list[k] += im.getpixel((j, i))[k]
+for l in range(0, 3):
+    return_list[l] = return_list[l] // (96*128)
+print(return_list)
+'''
+
+def main():
+    cam = Camob
+    cam.print_color_avg()
