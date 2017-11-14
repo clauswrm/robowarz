dc7826411dcda39a17c61112f51c95acff6646d8
Henfau <henrik.fauskanger@gmail.com>
2017-11-14 09:14:24 +0100

    Linefollowertest tweaks

--- basic_robot/sensobtests/lineFollowerTest.py
+++ basic_robot/sensobtests/lineFollowerTest.py
@@ -9,11 +9,13 @@ m = Motob()
 #TODO: Uten å bruke motorer, få den til å printe ut antall grader den vil svinge i hver retning ut i fra senorverdier
 
 def calculate_error(sens_values):
+    #returnerer en verdi som sier noe om hvor "feil" vi ligger på linja. En positiv verdi indikerer at vi er for langt
+    #til høyre. En negativ verdi indikerer at vi er for langt til venstre.
     outValue = 0
     outValue += sens_values[0]
     outValue += 0.6 * sens_values[1]
-    outValue -= 0.6 * sens_values[3]
-    outValue -= sens_values[4]
+    outValue -= 0.6 * sens_values[4]
+    outValue -= sens_values[5]
     return outValue
 
 
@@ -32,9 +34,11 @@ while True:
         if -0.1 < error < 0.1:
             pass
         elif error > 0:
-            m.turn_left(error * 20)
+            #m.turn_left(error * 20)
+            pass
         else:
-            m.turn_right(-error * 20)
+            #m.turn_right(-error * 20)
+            pass
         print(error)
     m.stop()
 
