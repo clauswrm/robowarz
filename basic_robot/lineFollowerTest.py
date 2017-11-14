26bfd4cbc60a2ac75223f981e310b8b43e6e8674
Henfau <henrik.fauskanger@gmail.com>
2017-11-14 11:37:37 +0100

    Hopefully fix dumb mistake

--- basic_robot/lineFollowerTest.py
+++ basic_robot/lineFollowerTest.py
@@ -39,10 +39,10 @@ while True:
             sleep(0.1)
             m.stop()
             if error > 0.1:
-                m.turn_left(error*20)
+                m.turn_right(error*20)
                 pass
             elif error <-0.1:
-                m.turn_right(-error*20)
+                m.turn_left(-error*20)
                 pass
             m.forward()
     except:
