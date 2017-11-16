69892b3cde5666479509e52552ca2053c599d6ed
Claus Martinsen <clauswrm@gmail.com>
2017-11-16 09:38:03 +0100

    Small changes to activation

--- basic_robot/ai/Turn_around.py
+++ basic_robot/ai/Turn_around.py
@@ -8,17 +8,15 @@ class TurnAround(Behavior):
         super().__init__(bbcon, priority)
         self.camera = camera
 
-
     def consider_activation(self):
         if not self.bbcon.moving:
             self.active_flag = True
-
 
     def consider_deactivation(self):
         if self.bbcon.moving:
             self.active_flag = False
 
-
     def sense_and_act(self):
         if self.camera.match_degree():
-            #TODO: Calculate weight for when in a garage and seeing red/green stuff
+            pass
+            # TODO: Calculate weight for when in a garage and seeing red/green stuff
