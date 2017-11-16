41b8f58832d1ed9b02bd204640207d47361acead
Claus Martinsen <clauswrm@gmail.com>
2017-11-16 09:12:55 +0100

    Small changes to sensors

--- basic_robot/sensobs/Proximity_Sensob.py
+++ basic_robot/sensobs/Proximity_Sensob.py
@@ -1,15 +1,12 @@
 from basic_robot.sensobs.irproximity_sensor import IRProximitySensor
+from basic_robot.sensobs.Sensob import Sensob
 from basic_robot.sensobs.ultrasonic import Ultrasonic
 
-class Proximity_Sensob:
+class Proximity_Sensob(Sensob):
 
     def __init__(self, ir_sensor: IRProximitySensor):
         self.ir_sensor = IRProximitySensor()
-        self.value = None
 
     def update(self):
         self.value = (self.ir_sensor.update() == [True, True])
-        return self.value
-
-    def get_value(self):
         return self.value
--- basic_robot/sensobs/Sensob.py
+++ basic_robot/sensobs/Sensob.py
@@ -1,6 +1,10 @@
 class Sensob:
     def __init__(self, sensor):
         self.sensor = sensor
+        self.value = None
 
     def update(self):
         self.sensor.update()
+
+    def get_value(self):
+        return self.value
