f137f1ccb5ff02cddc73a03e5afa56d00ca5290d
Claus Martinsen <clauswrm@gmail.com>
2017-11-16 08:59:06 +0100

    Reworked core behaviors and made the halt behavior

---
+++ basic_robot/ai/Turn_around.py
@@ -0,0 +1,24 @@
+from basic_robot.ai.Behaviors import Behavior
+from basic_robot.ai.BBCON import BBCON
+from basic_robot.sensobs.camobs import Camobs
+
+
+class TurnAround(Behavior):
+    def __init__(self, bbcon: BBCON, priority: int, camera: Camobs):
+        super().__init__(bbcon, priority)
+        self.camera = camera
+
+
+    def consider_activation(self):
+        if not self.bbcon.moving:
+            self.active_flag = True
+
+
+    def consider_deactivation(self):
+        if self.bbcon.moving:
+            self.active_flag = False
+
+
+    def sense_and_act(self):
+        if self.camera.match_degree():
+            #TODO: Calculate weight for when in a garage and seeing red/green stuff
