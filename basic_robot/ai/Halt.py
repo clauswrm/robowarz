fb3328d37436d9429d53223ef2532d23361cce38
Claus Martinsen <clauswrm@gmail.com>
2017-11-16 10:02:42 +0100

    Sensor and match degree changes

--- basic_robot/ai/Halt.py
+++ basic_robot/ai/Halt.py
@@ -7,12 +7,11 @@ class Halt(Behavior):
     def __init__(self, bbcon: BBCON, priority: int, prox_sensob: Proximity_Sensob):
         super().__init__(bbcon, priority)
         self.prox_sensob = prox_sensob
+        self.motor_recommendations = ['stop']
 
     def consider_activation(self):
         if self.bbcon.moving:
             self.active_flag = True
 
     def sense_and_act(self):
-        if self.prox_sensob.get_value():
-            pass
-            # TODO: Calculate weight for when in a garage
+        self.match_degree = 1 if self.prox_sensob.get_value() else 0
