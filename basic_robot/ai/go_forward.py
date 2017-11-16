d10c3692d0e08da4efd54f6eb21bbc945249e42f
Claus Martinsen <clauswrm@gmail.com>
2017-11-16 12:18:08 +0100

    Go-forward returns recommendation

--- basic_robot/ai/go_forward.py
+++ basic_robot/ai/go_forward.py
@@ -4,5 +4,8 @@ from basic_robot.ai.Behaviors import Behavior
 class Go_Forward(Behavior):
     def __init__(self, bbcon, priority):
         super().__init__(bbcon, priority)
-
+        self.motor_recommendations = ['forward']
         self.match_degree = 1
+
+    def sense_and_act(self):
+        return self.motor_recommendations
